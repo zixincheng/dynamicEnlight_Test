2de844bb673b70810c69f273c544c47f73d052c4
zixinc <jz_zx@hotmail.com>
2016-07-14 16:28:27 -0700

    Initial Commit

---
+++ dynamicTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamicTest
+//
+//  Created by zixin cheng on 2016-07-14.
+//  Copyright © 2016 zixin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
